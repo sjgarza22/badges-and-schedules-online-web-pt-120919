cb5e49ea4ac956d9513105879c7007e4c302ef1c
Sara Garza <sjgarza22@gmail.com>
2019-12-18 04:35:06 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,15 @@
 # Write your code here.
+def badge_maker(name)
+  return "Hello, my name is #{name}"
+end
+
+def batch_badge_creator(guest_speakers)
+  speakers_badges = []
+  guest_speakers.each do |name|
+    speakers_badges << badge_maker(name)
+  end
+end
+
+def assign_rooms(guest_speakers)
+
+end
